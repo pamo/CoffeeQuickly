e2416ec6fddc5969fe1918ed13bbc3ec342dcdef
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-10-26 08:45:48 -0700

    SoHo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
-      @lat = 37.770820
-      @long = -122.423736
+      # @lat = 37.770820
+      # @long = -122.423736
+      @lat = 40.733556
+      @long = -73.991520
       client = Foursquare2::Client.new(:client_id => ENV["client_id"], :client_secret => ENV["client_secret"])
       search = client.search_venues(:ll => "#{@lat},#{@long}", :query => 'coffee', :sortByDistance =>'1' )
       @coffee_venues = search.groups[0].items
