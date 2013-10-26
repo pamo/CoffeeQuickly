e88ee7316f11709259b00f2be4630ed734bb0ed6
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-10-26 08:15:14 -0700

    Show distance away

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,8 @@ class HomeController < ApplicationController
       search = client.search_venues(:ll => "#{@lat},#{@long}", :query => 'coffee', :sortByDistance =>'1' )
       @coffee_venues = search.groups[0].items
       @coffee_venues.each do |v|
+      	v.distance = v.location.distance * 0.00062137
+      	v.distance = v.distance.round(3)
       	puts v.categories.first.name unless v.categories.first.nil?
       	if v.count <= 9 
       		v.icon = "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
