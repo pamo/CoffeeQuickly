8eb9bdb73b24c086f7ee43c02877eb2e794660f4
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-10-26 01:26:35 -0700

    Config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoffeeQuickly::Application.config.secret_key_base = '8d243c47df68c371312dd808222a4c423f433d300ce6443b535175eb04182ae5f3e0d2c2e0a82ff6f9b19630a2bca47ed3ee1bab5e588b29c5fea2427da97f7c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoffeeQuickly::Application.config.session_store :cookie_store, key: '_CoffeeQuickly_session'
