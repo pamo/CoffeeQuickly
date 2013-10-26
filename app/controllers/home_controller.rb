b629d374667986e50f1ac492b11bf3e6ca63dbc0
Pamela Ocampo <pamela.ocampo@gmail.com>
2013-10-26 03:06:24 -0700

    Get all coffee venues near me

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
       @lat = 37.42
       @long = -122.07
       client = Foursquare2::Client.new(:client_id => ENV["client_id"], :client_secret => ENV["client_secret"])
-      @coffee = client.search_venues(:ll => "#{@lat},#{@long}", :query => 'coffee')
-      puts @coffee[0]
+      search = client.search_venues(:ll => "#{@lat},#{@long}", :query => 'coffee')
+      @coffee_venues = search.groups[0].items
   end
 end
